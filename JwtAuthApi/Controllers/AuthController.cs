88f5c00c7c7dd0c085a7ee7dccb77d002a4384b0
Fazliddin Nabiev <fazliddin.nabiev@ventionteams.com>
2024-05-02 23:06:03 +0500

    add AuthController

---
+++ JwtAuthApi/Controllers/AuthController.cs
@@ -0,0 +1,39 @@
+﻿using JwtAuthApi.core.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JwtAuthApi.Controllers;
+
+/// <summary>
+/// 
+/// </summary>
+
+[ApiController]
+[Route("[controller]")]
+public class AuthController() : ControllerBase
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="registrationDetails"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    [HttpPost]
+    [Route("signUp")]
+    public Task SignUp(RegistrationDto registrationDetails)
+    {
+        throw new NotImplementedException();
+    }
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="userDetails"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    [HttpGet]
+    [Route("signIn")]
+    public Task SignIn(LogInDto userDetails)
+    {
+        throw new NotImplementedException();
+    }
+}
