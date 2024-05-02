08972582bdb3567b09564b86ea735b5372faedc1
Fazliddin Nabiev <fazliddin.nabiev@ventionteams.com>
2024-05-02 23:10:45 +0500

    add Auth service

---
+++ JwtAuthApi/core/Interfaces/IAuthService.cs
@@ -0,0 +1,23 @@
+﻿using JwtAuthApi.core.Dtos;
+
+namespace JwtAuthApi.core.Interfaces;
+
+/// <summary>
+/// 
+/// </summary>
+public interface IAuthService
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="registrationDetails"></param>
+    /// <returns></returns>
+    Task RegisterUserAsync(RegistrationDto registrationDetails);
+    
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="logInDetails"></param>
+    /// <returns></returns>
+    Task<string> LogInAsync (LogInDto logInDetails);
+}
---
+++ JwtAuthApi/core/Services/AuthService.cs
@@ -0,0 +1,31 @@
+﻿using JwtAuthApi.core.Dtos;
+using JwtAuthApi.core.Interfaces;
+
+namespace JwtAuthApi.core.Services;
+
+/// <summary>
+/// 
+/// </summary>
+public class AuthService : IAuthService
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="registrationDetails"></param>
+    /// <exception cref="NotImplementedException"></exception>
+    public async Task RegisterUserAsync(RegistrationDto registrationDetails)
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="logInDetails"></param>
+    /// <returns></returns>
+    /// <exception cref="NotImplementedException"></exception>
+    public async Task<string> LogInAsync(LogInDto logInDetails)
+    {
+        throw new NotImplementedException();
+    }
+}
