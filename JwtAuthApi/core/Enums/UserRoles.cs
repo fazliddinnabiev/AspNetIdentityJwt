bab8636b36180d131723dce9c153d857aff4d259
Fazliddin Nabiev <fazliddin.nabiev@ventionteams.com>
2024-05-03 21:32:04 +0500

    add user roles enum

---
+++ JwtAuthApi/core/Enums/UserRoles.cs
@@ -0,0 +1,17 @@
+﻿namespace JwtAuthApi.core.Enums;
+
+/// <summary>
+/// Represents available user roles.
+/// </summary>
+public enum UserRoles
+{
+    /// <summary>
+    /// Role1
+    /// </summary>
+    Role1,
+
+    /// <summary>
+    /// Role2
+    /// </summary>
+    Role2,
+}
