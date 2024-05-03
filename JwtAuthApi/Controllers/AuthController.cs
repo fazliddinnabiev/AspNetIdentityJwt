f92781728973ae5fe404a112708f2b8363e00de9
Fazliddin Nabiev <fazliddin.nabiev@ventionteams.com>
2024-05-03 10:57:12 +0500

    write xml docs for AppDbContext.cs and AuthController.cs

--- JwtAuthApi/Controllers/AuthController.cs
+++ JwtAuthApi/Controllers/AuthController.cs
@@ -5,17 +5,17 @@ using Microsoft.AspNetCore.Mvc;
 namespace JwtAuthApi.Controllers;
 
 /// <summary>
-/// 
+/// Represents endpoint related to authentication.
 /// </summary>
 [ApiController]
 [Route("[controller]")]
-public class AuthController(IAuthService authService) : ControllerBase
+public sealed class AuthController(IAuthService authService) : ControllerBase
 {
     /// <summary>
-    /// Register user with given information.
+    /// Represents an API endpoint for user registration.
     /// </summary>
-    /// <param name="registrationDetails"><see cref="RegistrationDto"/></param>
-    /// <returns></returns>
+    /// <param name="registrationDetails">he registration details provided by the user.<see cref="RegistrationDto"/></param>
+    /// <returns>result of registration whether succeeded or failed.</returns>
     [HttpPost]
     [Route("signUp")]
     public Task SignUp(RegistrationDto registrationDetails)
@@ -24,16 +24,14 @@ public class AuthController(IAuthService authService) : ControllerBase
     }
 
     /// <summary>
-    /// 
+    /// Represents an API endpoint for user sign-in.
     /// </summary>
-    /// <param name="userDetails"></param>
-    /// <returns></returns>
-    /// <exception cref="NotImplementedException"></exception>
+    /// <param name="userDetails">The login details provided by the user.</param>
+    /// <returns>string that represents JWT token.</returns>
     [HttpGet]
     [Route("signIn")]
-    public Task SignIn(LogInDto userDetails)
+    public Task<string> SignIn(LogInDto userDetails)
     {
-        authService.LogInAsync(userDetails);
-        throw new NotImplementedException();
+        return authService.LogInAsync(userDetails);
     }
 }
--- JwtAuthApi/infrastructure/AppDbContext.cs
+++ JwtAuthApi/infrastructure/AppDbContext.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 namespace JwtAuthApi.infrastructure;
 
 /// <summary>
-/// 
+/// Represents the application database context.
 /// </summary>
-/// <param name="options"></param>
+/// <param name="options">The options for configuring the context.</param>
 public class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext(options: options);
