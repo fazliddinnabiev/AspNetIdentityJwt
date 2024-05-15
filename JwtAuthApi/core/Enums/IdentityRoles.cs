f5fa339e9373e90225494e6f914fccb125e99a67
Fazliddin Nabiev <fazliddin.nabiev@ventionteams.com>
2024-05-15 20:17:27 +0500

    Change default values of IdentityRoles enum

--- JwtAuthApi/core/Enums/IdentityRoles.cs
+++ JwtAuthApi/core/Enums/IdentityRoles.cs
@@ -8,10 +8,10 @@ public enum IdentityRoles
     /// <summary>
     /// Role1 appropriate number representation is 0.
     /// </summary>
-    Role1,
+    Role1 = 2,
 
     /// <summary>
     /// Role2 appropriate number representation is 1.
     /// </summary>
-    Role2,
+    Role2 = 4,
 }
