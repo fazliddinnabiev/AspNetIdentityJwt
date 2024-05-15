a9cb495bed709a0c4f3ce5072596e4717fcfc06c
Fazliddin Nabiev <fazliddin.nabiev@ventionteams.com>
2024-05-15 15:43:53 +0500

    change IAuthService methods signature

--- JwtAuthApi/core/Interfaces/IAuthService.cs
+++ JwtAuthApi/core/Interfaces/IAuthService.cs
@@ -1,4 +1,5 @@
 ﻿using JwtAuthApi.core.Dtos;
+using JwtAuthApi.core.ServiceResult;
 
 namespace JwtAuthApi.core.Interfaces;
 
@@ -12,12 +13,12 @@ public interface IAuthService
     /// </summary>
     /// <param name="registrationDetails">User registration details. See <see cref="RegistrationDto"/> for more information.</param>
     /// <returns>True if registration is successful; otherwise, false.</returns>
-    Task<bool> RegisterUserAsync(RegistrationDto registrationDetails);
+    Task<BaseResult<bool>> RegisterUserAsync(RegistrationDto registrationDetails);
 
     /// <summary>
     /// Logs in a user asynchronously.
     /// </summary>
     /// <param name="logInDetails">User login details. See <see cref="LogInDto"/> for more information.</param>
     /// <returns>A JWT token upon successful login.</returns>
-    Task<string> LogInAsync(LogInDto logInDetails);
+    Task<BaseResult<string>> LogInAsync(LogInDto logInDetails);
 }
