b057e11c48e52a7b5808b8f3017b94b7040152c8
Fazliddin Nabiev <fazliddin.nabiev@ventionteams.com>
2024-05-15 20:20:22 +0500

    Add jwtOptions class
    For representing JWT Bearer configuration options

---
+++ JwtAuthApi/core/Options/JwtOptions.cs
@@ -0,0 +1,23 @@
+﻿namespace JwtAuthApi.core.Options
+{
+    /// <summary>
+    /// Represents Json Web Token options.
+    /// </summary>
+    public class JwtOptions
+    {
+        /// <summary>
+        /// Key used for token generation and validation.
+        /// </summary>
+        public required string Key { get; set; }
+
+        /// <summary>
+        /// Valid issuer of the JWT.
+        /// </summary>
+        public required string ValidIssuer { get; set; }
+
+        /// <summary>
+        /// Valid audience for the JWT.
+        /// </summary>
+        public required string ValidAudience { get; set; }
+    }
+}
