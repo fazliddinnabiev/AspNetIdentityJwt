3e859c5ce5e93d67612a29202b573796df982aa9
Fazliddin Nabiev <fazliddin.nabiev@ventionteams.com>
2024-05-25 18:16:19 +0500

    Add OperationFailedResult.cs result type

---
+++ JwtAuthApi/core/ServiceResult/OperationFailedResult.cs
@@ -0,0 +1,22 @@
+﻿using JwtAuthApi.core.Enums;
+
+namespace JwtAuthApi.core.ServiceResult;
+
+/// <summary>
+/// Represents a service operation result that indicates an operation has failed.
+/// This class inherits from the <see cref="ServiceResult{T}"/> base class.
+/// </summary>
+/// <typeparam name="T">The type of data returned by the service operation.</typeparam>
+public class OperationFailedResult<T> : ServiceResult<T>
+{
+    /// <inheritdoc />
+    public override ResultTypes ResultType => ResultTypes.OperationFailed;
+
+    /// <inheritdoc />
+    public override string Errors { get; }
+
+    /// <summary>
+    /// Returns default(T) because an operation failure does not produce any data.
+    /// </summary>
+    public override T Data { get; }
+}
